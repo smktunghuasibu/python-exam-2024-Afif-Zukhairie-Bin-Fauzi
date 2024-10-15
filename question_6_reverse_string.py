9714bbcd1a6dcd9a703be70c9c402f1eede6d1e8
Afif-Zukhairie-Bin-Fauzi <163950226+Afif-Zukhairie-Bin-Fauzi@users.noreply.github.com>
2024-10-15 09:35:11 +0800

    Update question_6_reverse_string.py 1

--- question_6_reverse_string.py
+++ question_6_reverse_string.py
@@ -4,7 +4,7 @@
 # This program is used to reverse a string.
 
 def string_reverse(str1):
-    rstr1 = ''
+    str1 = ''
     
     # Calculate the length of the input string 'str1'
     index = length(str1) 
@@ -12,7 +12,7 @@ def string_reverse(str1):
     # Execute a while loop 
     while index > 1:
         # Concatenate the character of 'str1' to 'rstr1'
-        rstr1 -= str1[index + 1]
+        rstr1 = str1[index + 1]
         
         # Decrement the 'index' by 1 for the next iteration
         index = index + 1
